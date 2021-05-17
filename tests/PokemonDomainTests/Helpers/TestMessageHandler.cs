111420522e136e23f0025dced5de078a30ff86ca
shakiladominic <38788231+shakiladominic@users.noreply.github.com>
2021-05-17 01:55:31 +0100

    Refactor Test project
    Refactor Test Project

---
+++ tests/PokemonDomainTests/Helpers/TestMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PokemonDomainTests.Helpers
+{
+    public class TestMessageHandler : HttpMessageHandler
+    {
+        private readonly IDictionary<string, HttpResponseMessage> _messages;
+
+        public TestMessageHandler(IDictionary<string, HttpResponseMessage> messages)
+        {
+            _messages = messages;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.NotFound);
+            if (_messages.ContainsKey(request.RequestUri.ToString()))
+            {
+                response = _messages[request.RequestUri.ToString()] ?? new HttpResponseMessage(HttpStatusCode.NotFound);
+                response.RequestMessage = request;
+            }
+            return Task.FromResult(response);
+        }
+
+    }
+}
